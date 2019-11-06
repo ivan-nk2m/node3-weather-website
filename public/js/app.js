04f05b7b1071a56442696103beb2ba451d72c77e
Ivan Fernandez <ifernandez@dev001s-MacBook-Pro.local>
2019-11-06 14:02:23 -0600

    Setuo for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
@@ -12,7 +11,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data)=> {
             if(data.error){
                 messageOne.textContent = data.error
